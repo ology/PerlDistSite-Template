f4ccf41e56bf02e322e4038d8d5f750e6d9e9559
Gene Boggs <gene@ology.net>
2023-04-08 01:29:13 -0700

    Add to example

--- mirror-dist-site.pl
+++ mirror-dist-site.pl
@@ -5,6 +5,8 @@
 # git clone https://github.com/ology/PerlDistSite-Template.git
 # cd PerlDistSite-Template
 # perl mirror-dist-site.pl --who You --user you --dest ~/repos/Blah-Blah/docs --dist Blah-Blah --manual Manual --abstract "Do All The Things"
+# cd ~/repos/Blah-Blah/docs
+# make clean ; make all
 
 use strict;
 use warnings;
