b002cefb51c6532e75786182ba7e152c1ee66856
Gene Boggs <gene@ology.net>
2023-04-08 01:13:18 -0700

    Fix example

--- mirror-dist-site.pl
+++ mirror-dist-site.pl
@@ -4,7 +4,7 @@
 # Example:
 # git clone https://github.com/ology/PerlDistSite-Template.git
 # cd PerlDistSite-Template
-# perl mirror-dist-site.pl --user you --dest ~/tmp/Blah-Blah/docs --dist Blah-Blah --manual Manual --abstract "Do All The Things"
+# perl mirror-dist-site.pl --user you --dest ~/repos/Blah-Blah/docs --dist Blah-Blah --manual Manual --abstract "Do All The Things"
 
 use strict;
 use warnings;
