323b4c56ab037ecede64c39fc1596fb6eca4f937
Gene Boggs <gene@ology.net>
2023-04-10 23:13:53 -0700

    Do everything in one script

--- clean-and-move.sh
+++ clean-and-move.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
+rm -rf ../../<% LC_DIST %>-<% LC_MANUAL %>/assets/
+rm -rf ../../<% LC_DIST %>-<% LC_MANUAL %>/<% LC_MANUAL %>/
+
 make clean
 make all
 mv -f docs/* ../../<% LC_DIST %>-<% LC_MANUAL %>/
